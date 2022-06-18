af63143319763cb204a26071732c1ccf97b4cd8f
Bratix <braticamarr@gmail.com>
2022-06-19 00:25:40 +0200

    CHeck

--- static/src/js/custom/chat.js
+++ static/src/js/custom/chat.js
@@ -6,7 +6,8 @@ $(function(){
         const user_id = JSON.parse(document.getElementById('user_id').textContent);
         $(".infinite-container-chat").scrollTop($(".infinite-container-chat")[0].scrollHeight);
 
-        let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+        //let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
+        let ws_scheme = 'ws';
         const chatSocket = new WebSocket(
             ws_scheme 
             + '://'
