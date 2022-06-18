781bc617994c9c4c1d5e4c88e0ac2ae73f605486
Bratix <braticamarr@gmail.com>
2022-06-19 00:15:42 +0200

    Routing fix for websocket

--- chat/routing.py
+++ chat/routing.py
@@ -5,4 +5,5 @@ from . import consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/(?P<pk>\w+)$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/(?P<pk>\w+)$', consumers.ChatConsumer.as_asgi()),
 ]
