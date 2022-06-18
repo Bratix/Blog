d16cb10f23edd5f720f3e60085b84555c34bc6c0
Bratix <braticamarr@gmail.com>
2022-06-19 00:35:42 +0200

    js fix

--- static/src/js/custom/chat.js
+++ static/src/js/custom/chat.js
@@ -6,8 +6,7 @@ $(function(){
         const user_id = JSON.parse(document.getElementById('user_id').textContent);
         $(".infinite-container-chat").scrollTop($(".infinite-container-chat")[0].scrollHeight);
 
-        //let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-        let ws_scheme = 'ws';
+        let ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
         const chatSocket = new WebSocket(
             ws_scheme 
             + '://'
